84955dba6e11f9881fe1c5ee15bacf6195ca4d53
Dipak Pawar <pawardipak482@gmail.com>
2023-06-19 22:49:42 +0530

    added the task model added data

--- apps/todos/admin.py
+++ apps/todos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from apps.todos.models import Task
 
 # Register your models here.
+admin.site.register(Task)
--- apps/todos/models.py
+++ apps/todos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=250)
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["id"]
