cfb34fcd8120aa71351c6708df6beb486f318a21
Dipak Pawar <pawardipak482@gmail.com>
2023-06-25 00:36:06 +0530

    changes in serializers and added requirements

--- apps/todos/serializers.py
+++ apps/todos/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 
 class TaskSerializers(serializers.ModelSerializer):
+    # user = serializers.StringRelatedField()
+
     class Meta:
         model = Task
         exclude = ["created_at", "updated_at"]
