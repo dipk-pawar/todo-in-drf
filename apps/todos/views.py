58205e22bc48e594e8178f58d82eb22bb94a4b64
Dipak Pawar <pawardipak482@gmail.com>
2023-06-25 11:02:01 +0530

    added search filter

--- apps/todos/views.py
+++ apps/todos/views.py
@@ -2,12 +2,15 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 
 
 # Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializers
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title"]
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
--- todo_system/settings.py
+++ todo_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_filters",
     "apps.accounts",
     "apps.todos",
 ]
