030a4bc565b61d4fa4b9716f92c441af68a6df6d
Dipak Pawar <pawardipak482@gmail.com>
2023-07-23 14:45:58 +0530

    added the is_verified field

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -28,6 +28,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
